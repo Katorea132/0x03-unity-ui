819ea2742f9dcfec7b322dc8673c7f427762d49a
Katorea <katorea132@gmail.com>
2021-01-06 18:01:03 -0500

    Adding all the points all the way up to 12

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene("maze");
+    }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+    }
+}
